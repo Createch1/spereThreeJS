f05e7ec6779d6585bcc84100b23892d2dc4c4dff
ismaila2594 <ismaila.sagna@gmail.com>
2022-11-18 16:32:51 +0100

    modified:   propos.php

--- propos.php
+++ propos.php
@@ -20,6 +20,7 @@ include 'inc/nav.inc.php';
     <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
         <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+    
     </div>
     <div class="col-md-5 order-md-1">
         <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
