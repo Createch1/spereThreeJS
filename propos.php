966c74b92a09f2d792ce650c3692aefd9511df8a
ismaila2594 <ismaila.sagna@gmail.com>
2022-11-22 17:25:40 +0100

    new

--- propos.php
+++ propos.php
@@ -8,18 +8,30 @@ include 'inc/nav.inc.php';
 <div class="row featurette m-3">
     <div class="col-md-7">
         <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+        <p class="lead">Createch Informatique est une entreprise située en Seine-Saint-Denis (Tremblay-en-
+France). Notre expérience client et technique nous ont conduit à créer en toute logique
+Createch Informatique en proposant des services et prestations de qualité.
+Nous sommes spécialisés dans l’assistance, l’installation, le dépannage informatique et la
+création de sites web sur-mesure pour les particuliers et les professionnels (TPE, PME).
+De plus, nous assurons les réparations, la maintenance et le nettoyage de vos ordinateurs
+et périphériques ainsi que le conseil, l’infogérance et le montage de votre ordinateur.
+Nous sommes à votre service pour l’assistance et le dépannage de vos équipements
+informatiques qu’ils soient en pannes ou cassés. Nous pouvons intervenir sur la plus part
+des grandes marques du marché (Asus, Acer, HP, Lenovo, Toshiba, Dell….)</p>
     </div>
     <div class="col-md-5">
         <img src="https://images.prismic.io/govirtuo/bc0439a3-b3ff-4deb-9d0d-00e701b230c9_Virtuo_Drive_Free-62.jpg?auto=compress,format&w=1920&q=75" alt="" class="w-100 rounded">
-
     </div>
 </div>
 <hr class="featurette-divider">
+
+
 <div class="row featurette m-3">
     <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+        <p class="lead">CREATECH Informatique a été créée pour répondre aux différents besoins de satisfaire la clientèle et
+de contribuer à améliorer l’expérience client au travers du service à la personne, de conseils,
+dépannages informatique et création de sites web sur-mesure.</p>
     
     </div>
     <div class="col-md-5 order-md-1">
@@ -30,4 +42,26 @@ include 'inc/nav.inc.php';
 
     </div>
 </div>
+<hr class="featurette-divider">
+
+<div class="row featurette m-3">
+    <div class="col-md-7">
+        <h2 class="">Nos périmètres d’action </h2>
+        <p class="lead">De nos jours, l’outil informatique est de plus en plus utilisé que ce soit à titre personnel ou
+pour le travail. Avec l’épidémie du Covid-19, nous avons aussi dû nous adapter à travailler
+différemment notamment avec la mise en place du Télétravail partiel ou du Full-Télétravail ce
+qui peut provoquer un stress lorsque nos outils informatiques tombent en panne ou
+ralentissent.
+Afin de travailler dans de bonnes conditions et tenir ses objectifs personnels et professionnels,
+CREATECH Informatique intervient à domicile rapidement ou à distance dans le cas où la panne
+peut se solutionner par ce type d’intervention. Vous ne perdez alors plus de temps à rapporter
+votre ordinateur et vous êtes rapidement dépanner.</p>
+    </div>
+    <div class="col-md-5">
+        <img src="https://images.prismic.io/govirtuo/bc0439a3-b3ff-4deb-9d0d-00e701b230c9_Virtuo_Drive_Free-62.jpg?auto=compress,format&w=1920&q=75" alt="" class="w-100 rounded">
+    </div>
+</div>
 </main>
+
+<?php
+include 'inc/footer.inc.php';
