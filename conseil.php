8e56159d2149568590c7de9b59875822da381f4a
walidoum <116063148+walidoum@users.noreply.github.com>
2022-11-28 10:07:35 +0100

    walid

---
+++ conseil.php
@@ -0,0 +1,10 @@
+<?php
+include 'inc/init.inc.php';
+include 'inc/function.inc.php';
+include 'inc/header.inc.php';
+include 'inc/nav.index.inc.php';
+?> 
+
+<?php 
+include 'inc/footer.inc.php';
+?>
