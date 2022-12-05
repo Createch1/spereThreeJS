233a15dcdb1080ae7f5831421d1635d9f253e77e
walidoum <116063148+walidoum@users.noreply.github.com>
2022-12-05 11:11:50 +0100

    changement css

--- services.php
+++ services.php
@@ -4,7 +4,7 @@ include 'inc/function.inc.php';
 include 'inc/header.inc.php';
 include 'inc/nav.inc.php';
 ?>
-
+<!-- 
 <div class="page_services">
   <h1>Nos services</h1>
       <p class="first_p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio animi, quae eligendi ullam deleniti possimus distinctio commodi molestiae neque temporibus dicta iste, voluptatibus nesciunt qui debitis nisi blanditiis natus amet inventore nam? Voluptate optio atque assumenda, tempora ipsam tenetur harum aliquid deserunt aut soluta? Modi, quasi esse? Cupiditate, quod saepe! </p>
@@ -334,8 +334,130 @@ include 'inc/nav.inc.php';
           </div>                                                                        
         </div>
 </div>
-
-
+-->
+<div class="container">      
+      <div class="row row-cols-1 row-cols-lg-3 row-cols-md-2 g-1 pt-3 pb-5 mb-0 d-flex justify-content-evenly " >
+        <div class="col p-4">
+          <div class="card h-100 w-100">
+            <img src="https://cdn.pixabay.com/photo/2018/01/20/15/06/hard-drive-3094771_1280.jpg" class="card-img-top" >
+            <div class="card-body">
+              <h5 class="card-title">FORFAIT 1</h5>
+              <div class="card-text">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+              </div>                
+            </div>
+            <div class="bouton-en-bas">
+              <div class="button ">
+              <button type="button" class="btn btn-outline-primary justify-content-center" >À partir de 49,99€</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col p-4 ">
+          <div class="card h-100 w-100">
+            <img src="https://cdn.pixabay.com/photo/2016/04/04/14/12/monitor-1307227_1280.jpg" class="card-img-top" >
+            <div class="card-body">
+              <h5 class="card-title">FORFAIT 2</h5>
+              <div class="card-text">
+                <p>At vero eos et accusamus et CEST QUOI CE BORDEL ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>gejhrbfkjerjfejrngknerjfergrtget</p>
+                </div>               
+              </div>
+            <div class="bouton-en-bas">
+              <div class="button ">
+              <button type="button" class="btn btn-outline-primary justify-content-center" >À partir de 49,99€</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col p-4">
+          <div class="card h-100 w-100">
+            <img src="https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536__480.jpg" class="card-img-top" >
+            <div class="card-body">
+              <h5 class="card-title">FORFAIT 3</h5>
+              <div class="card-text">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                </div>
+                
+            </div>
+            <div class="bouton-en-bas">
+              <div class="button ">
+              <button type="button" class="btn btn-outline-primary justify-content-center" >À partir de 49,99€</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col p-4">
+          <div class="card h-100 w-100">
+            <img src="https://cdn.pixabay.com/photo/2017/10/10/21/47/laptop-2838921__480.jpg" class="card-img-top" >
+            <div class="card-body">
+              <h5 class="card-title">FORFAIT 4</h5>
+              <div class="card-text">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                </div>
+                
+            </div>
+            <div class="bouton-en-bas">
+              <div class="button ">
+              <button type="button" class="btn btn-outline-primary justify-content-center" >À partir de 49,99€</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col p-4">
+          <div class="card h-100 w-100">
+            <img src="https://cdn.pixabay.com/photo/2017/11/16/09/32/matrix-2953869__480.jpg" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">FORFAIT 5</h5>
+              <div class="card-text">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+              </div>
+              
+            </div>
+            <div class="bouton-en-bas">
+              <div class="button ">
+              <button type="button" class="btn btn-outline-primary justify-content-center" >À partir de 49,99€</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col p-4">
+          <div class="card h-100 w-100">
+            <img src="https://cdn.pixabay.com/photo/2014/08/26/21/27/service-428539__480.jpg" class="card-img-top" >
+            <div class="card-body">
+              <h5 class="card-title">FORFAIT 6</h5>
+              <div class="card-text">
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus</p>
+              </div>
+              
+            </div>
+            <div class="bouton-en-bas">
+              <div class="button ">
+              <button type="button" class="btn btn-outline-primary justify-content-center" >À partir de 49,99€</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 <?php 
 include 'inc/button.scroll.inc.php';
