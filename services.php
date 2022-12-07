904ace28d422cf46d6c4d114d87dce710fde986e
walidoum <116063148+walidoum@users.noreply.github.com>
2022-12-07 15:07:24 +0100

    walid

--- services.php
+++ services.php
@@ -10,13 +10,10 @@ include 'inc/nav.inc.php';
       <p class="first_p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio animi, quae eligendi ullam deleniti possimus distinctio commodi molestiae neque temporibus dicta iste, voluptatibus nesciunt qui debitis nisi blanditiis natus amet inventore nam? Voluptate optio atque assumenda, tempora ipsam tenetur harum aliquid deserunt aut soluta? Modi, quasi esse? Cupiditate, quod saepe! </p>
         <div class="sous-titre">
         Montage de PC sur mesure
-        </div>  
-            
+        </div>             
         <div class="grille">
           <div class="grid-item">
-            <div class="grid-image">
-
-            </div>
+            <img src="https://images.pexels.com/photos/4705635/pexels-photo-4705635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="grid-image">
             <div class="grid-title">
             Installation de composants PC et/ou périphériques PC
             </div>
@@ -26,26 +23,22 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grid-item">
             <div class="grid-image">
-
+            <img src="https://static6.depositphotos.com/1056393/584/i/600/depositphotos_5847868-stock-photo-pc-service.jpg" class="grid-image">
             </div>
             <div class="grid-title">
             Montage d’un ordinateur simple ou complexe 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
-            </div>
+           </div>
           </div>
           <div class="grid-item">
-            <div class="grid-image">
-
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Montage d’un ordinateur + Installation logiciel simple ou complexe 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div> 
           </div> 
@@ -54,75 +47,57 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille">
           <div class="grid-item">
-            <div class="grid-image">
-
-            </div>
+            <img src=" " class="grid-image">
             <div class="grid-title">
             Dépoussiérage 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           <div class="grid-item">
-            <div class="grid-image">
-            
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Réparation ordinateur / Forfait de prise en charge et diagnostic
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           <div class="grid-item">
-            <div class="grid-image">
-            
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Désinfection d’un ordinateur ( virus/malware )
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           <div class="grid-item">
-            <div class="grid-image">
-            
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             démontage / remontage 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           <div class="grid-item">
-            <div class="grid-image">
-            
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             démontage / remontage complexe 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           <div class="grid-item">
-            <div class="grid-image">
-            
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Diagnostic disque dur
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           </div> 
@@ -131,39 +106,29 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille">
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Optimisation des performances de l’ordinateur 
             </div>
             <div class="grid-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
-            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.             </div>
           </div> 
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Mises à jour du système d’exploitation, des pilotes du système et des logiciels
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div> 
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Détection et suppression de virus et de programmes malveillants
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div> 
           </div> 
@@ -172,27 +137,21 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille" >
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Dépoussiérage
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div> 
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Installation mémoire / disque dur
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           </div> 
@@ -201,39 +160,30 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille"> 
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Mise à jour d’un BIOS 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>            
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Installation d’un logiciel 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>            
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Installation de Windows ou d’un OS non Windows
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>     
           </div> 
@@ -242,27 +192,21 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille" >                  
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             30 minutes de main d’œuvre / Dépannage informatique
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>            
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Intervention express 48 H
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>
           </div> 
@@ -271,88 +215,66 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille">          
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Déblocage sessions Windows 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>            
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Installation box, CPL et répéteurs WIFI 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>            
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Déploiement et migration de postes de travail
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>    
-        
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Conseils et diagnostics avant achat 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>            
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Sites Web sur-mesure ( Site Vitrine, E-Commerce, Site Mobile, Communautaire, Institutionnel, Blog, Forum )
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic provident ut, error repellat magnam consequatur.
-
             </div>
           </div>                                                                                
           <div class="grid-item">
-            <div class="grid-image">
-            
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Sauvegarde et transfert de données
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>            
           <div class="grid-item">
-            <div class="grid-image">
-                
-            </div>
+          <img src=" " class="grid-image">
             <div class="grid-title">
             Réparation / Configuration imprimantes 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>                                                                        
         </div>
