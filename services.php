9a4b602d4bf7f787fef50d2f4f515c3cd18b6d80
walidoum <116063148+walidoum@users.noreply.github.com>
2022-12-13 13:56:17 +0100

    walid

--- services.php
+++ services.php
@@ -18,7 +18,7 @@ include 'inc/nav.inc.php';
             Installation de composants PC et/ou périphériques PC
             </div>
             <div class="grid-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
+            Envie de mettre à jour votre PC ou de lui adjoindre un nouveau périphérique ? Pas de stress : avec notre prestation d'installation de composant et/ou de périphérique, nos professionnels s'en occupent pour vous. (ne s'applique pas aux boîtiers et cartes mères)
             </div>
           </div>
           <div class="grid-item">
@@ -29,8 +29,7 @@ include 'inc/nav.inc.php';
             Montage d’un ordinateur simple ou complexe 
             </div>
             <div class="grid-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-            </div>
+            Ce sont nos techniciens passionnés qui prendront soin de chaque détail pour assembler votre PC sur mesure. C'est cette qualité de service que nous vous proposons pour le montage de votre ordinateur, qui sera ainsi assemblé et configuré selon vos besoins précis.</div>
           </div>
           <div class="grid-item">
           <img src="https://st2.depositphotos.com/3591429/11911/i/600/depositphotos_119114344-stock-photo-woman-working-with-laptop.jpg" class="grid-image">
@@ -38,7 +37,9 @@ include 'inc/nav.inc.php';
             Montage d’un ordinateur + Installation logiciel  
             </div>
             <div class="grid-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
+            Cette prestation comporte l'installation des composants de votre PC et du système (Partitions, Windows, drivers...). 
+            une demande particulière ? 
+            N'hésitez-pas à nous communiquer vos instructions spécifiques de montage lors du passage de votre commande.
             </div>
           </div> 
           </div> 
@@ -52,20 +53,29 @@ include 'inc/nav.inc.php';
             Dépoussiérage 
             </div>
             <div class="grid-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
+            Le dépoussiérage permet d'éliminer les poussières et les saletés accumulées à l'intérieur d'un ordinateur, ce qui peut affecter les performances de l'ordinateur et causer des problèmes tels que des surchauffes ou des plantages fréquents. CREATECH s'occupe de ça.
             </div>
           </div>
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://images.pexels.com/photos/10558599/pexels-photo-10558599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="grid-image">
             <div class="grid-title">
-            Réparation ordinateur / Forfait de prise en charge et diagnostic
+            Réparation ordinateur 
             </div>
             <div class="grid-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-            </div>
+            Une réparation d'ordinateur peut être nécessaire pour de nombreuses raisons, telles que des problèmes de logiciels, des pannes matérielles ou des performances dégradées. Dans ce cas, il est important de faire appel à un professionnel qualifié pour diagnostiquer le problème et proposer une solution adaptée. 
+          </div>
           </div>
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://images.pexels.com/photos/4792727/pexels-photo-4792727.jpeg?auto=compress&cs=tinysrgb&w=1200" class="grid-image">
+            <div class="grid-title">
+            Forfait de prise en charge et diagnostic
+            </div>
+            <div class="grid-text">
+            Le forfait de prise en charge et diagnostic permet de couvrir les frais liés à l'intervention d'un technicien pour identifier la cause du problème et préparer un devis pour les réparations nécessaires. Ce forfait peut inclure des tests et des analyses approfondies.
+          </div>
+          </div>
+          <div class="grid-item">
+          <img src="https://st2.depositphotos.com/1202020/6581/i/600/depositphotos_65811533-stock-photo-virus-alert.jpg" class="grid-image">
             <div class="grid-title">
             Désinfection d’un ordinateur ( virus/malware )
             </div>
@@ -74,7 +84,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://static8.depositphotos.com/1421957/1048/i/600/depositphotos_10483747-stock-photo-laptop-mother-board-fixing.jpg" class="grid-image">
             <div class="grid-title">
             démontage / remontage 
             </div>
@@ -83,7 +93,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st2.depositphotos.com/10614052/42219/i/600/depositphotos_422197472-stock-photo-african-american-technician-repairing-computer.jpg" class="grid-image">
             <div class="grid-title">
             démontage / remontage complexe 
             </div>
@@ -92,7 +102,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st3.depositphotos.com/13053202/16913/i/600/depositphotos_169130102-stock-photo-male-repairman-wearing-blue-gloves.jpg" class="grid-image">
             <div class="grid-title">
             Diagnostic disque dur
             </div>
@@ -106,7 +116,7 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille">
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st3.depositphotos.com/1001201/16538/i/600/depositphotos_165384664-stock-photo-fast-internet-concept.jpg" class="grid-image">
             <div class="grid-title">
             Optimisation des performances de l’ordinateur 
             </div>
@@ -114,7 +124,7 @@ include 'inc/nav.inc.php';
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.             </div>
           </div> 
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st4.depositphotos.com/14431644/30150/i/600/depositphotos_301507446-stock-photo-handwriting-text-update-concept-meaning.jpg" class="grid-image">
             <div class="grid-title">
             Mises à jour du système d’exploitation, des pilotes du système et des logiciels
             </div>
@@ -123,7 +133,7 @@ include 'inc/nav.inc.php';
             </div>
           </div> 
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st.depositphotos.com/1152339/1950/i/600/depositphotos_19500391-stock-photo-privacy-concept-on-digital-background.jpg" class="grid-image">
             <div class="grid-title">
             Détection et suppression de virus et de programmes malveillants
             </div>
@@ -137,7 +147,7 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille" >
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st3.depositphotos.com/1177973/15825/i/600/depositphotos_158253610-stock-photo-man-cleaning-laptop-cooler-with.jpg" class="grid-image">
             <div class="grid-title">
             Dépoussiérage
             </div>
@@ -146,7 +156,7 @@ include 'inc/nav.inc.php';
             </div>
           </div> 
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st4.depositphotos.com/22662890/31014/i/600/depositphotos_310148378-stock-photo-installing-a-solid-state-drive.jpg" class="grid-image">
             <div class="grid-title">
             Installation mémoire / disque dur
             </div>
@@ -160,7 +170,7 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille"> 
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st3.depositphotos.com/4012225/18720/i/600/depositphotos_187201610-stock-photo-first-running-of-pc-bios.jpg" class="grid-image">
             <div class="grid-title">
             Mise à jour d’un BIOS 
             </div>
@@ -169,7 +179,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>            
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st4.depositphotos.com/13754004/22503/i/600/depositphotos_225031212-stock-photo-man-installing-software-laptop-dark.jpg" class="grid-image">
             <div class="grid-title">
             Installation d’un logiciel 
             </div>
@@ -178,7 +188,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>            
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st4.depositphotos.com/1001860/22638/i/600/depositphotos_226389788-stock-photo-montreal-canada-november-2018-windows.jpg" class="grid-image">
             <div class="grid-title">
             Installation de Windows ou d’un OS non Windows
             </div>
@@ -192,7 +202,7 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille" >                  
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://static3.depositphotos.com/1007056/221/i/600/depositphotos_2216918-stock-photo-repair-a-computer-surface-mounted.jpg" class="grid-image">
             <div class="grid-title">
             30 minutes de main d’œuvre / Dépannage informatique
             </div>
@@ -201,7 +211,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>            
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st.depositphotos.com/1002881/1316/i/600/depositphotos_13167382-stock-photo-48h-delivery.jpg" class="grid-image">
             <div class="grid-title">
             Intervention express 48 H
             </div>
@@ -215,7 +225,7 @@ include 'inc/nav.inc.php';
           </div>
           <div class="grille">          
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st2.depositphotos.com/1252160/8278/i/600/depositphotos_82786086-stock-photo-hands-using-windows-10-on.jpg" class="grid-image">
             <div class="grid-title">
             Déblocage sessions Windows 
             </div>
@@ -224,7 +234,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>            
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st.depositphotos.com/1546680/3057/i/600/depositphotos_30576179-stock-photo-internet-routers.jpg" class="grid-image">
             <div class="grid-title">
             Installation box, CPL et répéteurs WIFI 
             </div>
@@ -233,25 +243,16 @@ include 'inc/nav.inc.php';
             </div>
           </div>            
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://static5.depositphotos.com/1037987/479/i/600/depositphotos_4797043-stock-photo-view-of-stock-trading-office.jpg" class="grid-image">
             <div class="grid-title">
             Déploiement et migration de postes de travail
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
             </div>
-          </div>    
+          </div>
           <div class="grid-item">
-          <img src=" " class="grid-image">
-            <div class="grid-title">
-            Conseils et diagnostics avant achat 
-            </div>
-            <div class="grid-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-            </div>
-          </div>            
-          <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st3.depositphotos.com/3591429/15092/i/600/depositphotos_150927082-stock-photo-woman-using-laptop.jpg" class="grid-image">
             <div class="grid-title">
             Sites Web sur-mesure ( Site Vitrine, E-Commerce, Site Mobile, Communautaire, Institutionnel, Blog, Forum )
             </div>
@@ -260,7 +261,7 @@ include 'inc/nav.inc.php';
             </div>
           </div>                                                                                
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st.depositphotos.com/1187563/3333/i/600/depositphotos_33333557-stock-photo-two-laptop-computers-with-folders.jpg" class="grid-image">
             <div class="grid-title">
             Sauvegarde et transfert de données
             </div>
@@ -269,13 +270,12 @@ include 'inc/nav.inc.php';
             </div>
           </div>            
           <div class="grid-item">
-          <img src=" " class="grid-image">
+          <img src="https://st3.depositphotos.com/1192060/13012/i/600/depositphotos_130125318-stock-photo-fixing-a-printer-and-clipped.jpg" class="grid-image">
             <div class="grid-title">
             Réparation / Configuration imprimantes 
             </div>
             <div class="grid-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque debitis non atque velit inventore? Id eligendi culpa veritatis. In neque, nam suscipit impedit earum illum sequi obcaecati veniam a, quis hic numquam incidunt maxime provident ut, error repellat magnam consequatur.
-
             </div>
           </div>                                                                        
         </div>
