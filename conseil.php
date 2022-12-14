3aa9bf9b6c55f9e4a61e2480c39271a1aeb00448
ismaila2594 <ismaila.sagna@gmail.com>
2022-12-14 22:58:19 +0100

    Update conseil.php

--- conseil.php
+++ conseil.php
@@ -4,7 +4,142 @@ include 'inc/function.inc.php';
 include 'inc/header.inc.php';
 include 'inc/nav.inc.php';
 ?> 
+<main class="container">
+<h3>FAQ</h3>
+<h4>Vous avez des questions ? Et vous souhaitez avoir plus de precision sur votre demande ?</h4>
+<p>Veuillez verifier si votre question n'est pas parmis les question ci-dessous, sinon veullez nous fairevotres demande dans la page contact <!-- lien vers page contact--></p>
 
+
+<div class="accordion p-4" id="myAccordion">
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingOne">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseOne">QU'EST CE QUE LA RESTAURATION DU SYSTÈME?</button>									
+            </h5>
+            <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>La Restauration du système vous aide à restaurer les fichiers système de votre ordinateur à un point antérieur dans le temps. <br>
+
+Cela permet d'annuler les modifications du système sans affecter vos fichiers personnels, tels que le courrier électronique, les documents ou les photos.
+</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingTwo">
+                <button type="button" class="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapseTwo">COMMENT SE CONNECTER À UN RÉSEAU WIFI?</button>
+            </h5>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>Il est parfois utile de se connecter à internet sans fil.
+Nous allons vous expliquer comment en profiter.
+Pour effectuer la connexion de votre ordinateur en wifi vous pouvez suivre la procédure guidée de Windows.
+Cette opération est extrêmement rapide.
+Tout ce que vous avez à faire, est de suivre les étapes proposées et d'entrer les données qui vous sont demandées. <br> 
+Affichez les réseaux wifi disponibles
+Pour afficher les réseaux sans fil à proximité de votre ordinateur, cliquez sur l'icône wifi qui se situe au coin inferieur droit de votre écran (à côté de l'horloge Windows, voir image ci-dessous).<br>
+<img src="" alt="reseau"> <br><br>
+ 
+
+Retrouvez votre réseau
+En cliquant de nouveau avec le bouton droit sur l'icône des réseaux de Windows, choisissez de vous connecter à votre réseau. Si vous avez caché le SSID du réseau, choisissez la connexion manuelle et spécifiez dès à présent son nom.
+
+
+</p>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingThree">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree">PROTÉGER SES ENFANTS SUR INTERNET</button>                     
+            </h5>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>Internet est un outil fabuleux, rapidement devenu incontournable dans notre vie quotidienne.<br>Nos enfants montrent généralement une grande aisance dans l’usage de cet outil, mais il est nécessaire de les aider à en avoir une utilisation raisonnée, responsable et sûre. C’est par ailleurs un formidable levier au service des apprentissages.<br><br>
+
+                    La solution qui vous permet de laisser surfer vos enfants en toute liberté !<br><br>
+ 
+Internet est un très bon moyen pour les enfants d'apprendre, de discuter avec des amis ou de tout simplement se divertir. Mais comme dans la vie de tous les jours, internet peut être un danger pour vos enfants !<br>
+
+Avant de laisser vos enfants naviguer seuls sur internet, il est impératif d'avertir vos enfants sur les dangers possibles du web.<br><br>
+
+Avant l’installation d'un contrôle parental:
+<br>
+Assurez-vous que chacun de vos enfants utilise un compte utilisateur différent pour ouvrir une session sur l’ordinateur. En effet, les contrôles parentaux identifient la personne qui utilise l’ordinateur selon le compte utilisé pour la connexion.
+
+                  </p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingFour">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseFour">COMMENT RÉAGIR FACE À UNE INFECTION?</button>                     
+            </h5>
+            <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>Si vos mesures préventives n’ont pas étaient suffisantes pour vous éviter une cyberattaque, il reste tout de même possible de limiter les dégâts en appliquant quelques conseils simples.<br>
+Grâce à ces simples reflexes, vous pourrez limiter les dégâts causés le programme malveillant en attendant l’intervention de notre service de dépannage informatique.<br>
+
+Gardez votre calme
+<br>
+En premier lieu, nous vous conseillons de ne pas céder à la panique.
+<strong> Réagir sous la pression, ou la peur peut vous amener à faire de mauvaises manipulations</strong> <br><br>
+Eteignez votre ordinateur
+<br><br>
+Pour mener à terme ses taches, tout programme informatique a besoin que l’ordinateur soit sous tension (et connecté au réseau dans certains cas).
+En cas d’infection suspectée ou avérée, votre premier réflexe sera d’éteindre l’ordinateur et de le déconnecter du réseau.
+Le programme malicieux ne pouvant finir son travail, l’onde de choc pourra ainsi être limitée.
+Attention : Certains malwares vous affiche un numéro de téléphone à appeler accompagné d’une alerte audio.
+Il s’agit d’un piège permettant aux pirates de mieux manipuler la victime par téléphone en lui faisant installer des programmes malicieux. Par sécurité, nous vous recommandons donc de ne pas contacter ce numéro.
+Laissez votre ordinateur éteint en attendant l’intervention de notre expert en sécurité informatique, en vue d’augmenter les chances de récupération de données et de limiter la propagation du virus.
+Sécurisez vos services en ligne
+Certains malwares ont pour fonction de récupérer des identifiants web. Parmi les accès les plus ciblés, nous retrouvons les identifiants bancaire, les numéros de CB, mais aussi les accès au cloud (dropbox, icloud, etc…) ou les boites mails.
+Afin d’éviter altération ou un accès non autorisé à vos services en ligne ou compte bancaire, nous vous conseillons de changer vos mots de passes depuis un ordinateur sain en urgence.
+
+Stoppez la propagation de l’infection via les supports amovibles
+Pour éviter d’infecter un autre ordinateur ou de réinfecter le vôtre, il est essentiel de regrouper tous les disques dur externes ou clés USB ayant été récemment connectés au poste infecté. Nous vous recommandons de ne pas utiliser ces supports amovibles avant qu’ils n’aient été nettoyés. Lors de l’intervention, notre expert en cyber sécurité pourra procéder à la désinfection de vos supports et votre ordinateur.
+</p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingFive">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseFive">COMMENT CHOISIR UN MOT DE PASSE SOLIDE?</button>                     
+            </h5>
+            <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>Des techniques existent pour tenter de casser les mots de passe.
+La plus utilisée consiste à faire des essais systématiques à partir de dictionnaires : on connaît l’algorithme de codage des mots de passe, il suffit alors de l’appliquer à des dictionnaires choisis astucieusement  sur internet, et de comparer le résultat à chacune des entrées du fichier système contenant les mots de passe qu’on a réussi à extraire au préalable.
+Par cette technique, on arrive à casser en moyenne plus de 20% des mots de passe d’un fichier en moins d’une heure.
+La loi de composition d’un bon mot de passe doit rendre cette technique inefficace, d’où les règles suivantes :
+ 
+Règle 1 : Un mot de passe ne doit pas pouvoir être trouvé dans un dictionnaire.
+Les deux autres techniques utilisées, consistent à essayer toutes les combinaisons possibles, soit sur un jeu réduit de caractères, soit en cherchant une chaîne de caractères de petite longueur. Pour faire échouer ces tentatives, il faut élargir au maximum le champ des combinaisons possibles, ce qui conduit à énoncer les deux règles suivantes. 
+ 
+Règle 2 :Votre mot de passe doit contenir un mélange de caractères alphanumériques et de caractères spéciaux (- + ! § %, ...).
+ 
+Règle 3 : Votre mot de passe doit faire au moins 8 caractères (sur les systèmes Unix, seuls les 8 premiers caractères sont pris en considération).
+ 
+Ne prêtez pas votre mot de passe.
+Un mot de passe est un secret entre vous et votre machine qui ne doit être partagé par personne d’autre. Si vous le confiez à quelqu’un, même à votre étudiant, à votre ami ou encore à un proche, ce n’est plus un secret et le mot de passe ne joue plus son rôle d’authentifiant. Vous mettez en échec la sécurité du système dans son fondement ; dès lors, toutes les mesures que vous pourriez prendre par ailleurs, ne servent plus à rien.
+ 
+N'écrivez pas votre mot de passe.
+Votre mot de passe ne doit pas être inscrit sur un support, à proximité de la machine ou de manière qu’un rapprochement puisse être fait avec le système qu’il est censé protéger. Les « stickers » sous le clavier ou le tapis de la souris, ne sont pas une bonne idée !
+ 
+Changez réguliérement votre mot de passe.
+Les mots de passe circulent en clair sur les réseaux. Des techniques simples (sniffers, espions, chevaux de Troie ...), peuvent être mises en oeuvre pour capter le couple (identifiant, mot de passe) à l’insu des utilisateurs et administrateurs. Ces dispositifs peuvent rester en place pendant des mois avant d’être découverts. Pendant ce temps, tapis à l’écoute du réseau, ils captent tous les mots de passe qui circulent.
+
+C’est pourquoi, même robuste, un mot de passe doit être modifié régulièrement - au moins tous les trois mois. Mais cette exigence pose un problème de mémorisation, qui devient insurmontable lorsqu’on a plusieurs mots de passe à se rappeler et qu’on applique scrupuleusement les règles ci-dessus. C’est pourquoi un mot de passe ne peut être un pur aléa. Il faut avoir une règle de constitution mémotechnique.
+</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</main>
 <?php 
 include 'inc/footer.inc.php';
 ?>
