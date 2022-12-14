6d5f9766738055687018218b4897bad95258762d
ismaila2594 <ismaila.sagna@gmail.com>
2022-12-14 13:46:58 +0100

    Update faq.php

--- faq.php
+++ faq.php
@@ -11,7 +11,81 @@ include 'inc/nav.inc.php';
 <h4>Vous avez des questions ? Et vous souhaitez avoir plus de precision sur votre demande ?</h4>
 <p>Veuillez verifier si votre question n'est pas parmis les question ci-dessous, sinon veullez nous fairevotres demande dans la page contact <!-- lien vers page contact--></p>
 
-<div class="accordion accordion-flush mb-3" id="accordionFlushExample">
+
+<div class="accordion p-4" id="myAccordion">
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingOne">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseOne">QU'EST CE QUE LA RESTAURATION DU SYSTÈME?</button>									
+            </h5>
+            <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>La Restauration du système vous aide à restaurer les fichiers système de votre ordinateur à un point antérieur dans le temps. <br>
+
+Cela permet d'annuler les modifications du système sans affecter vos fichiers personnels, tels que le courrier électronique, les documents ou les photos.
+</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingTwo">
+                <button type="button" class="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapseTwo">COMMENT SE CONNECTER À UN RÉSEAU WIFI?</button>
+            </h5>
+            <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>Il est parfois utile de se connecter à internet sans fil.
+Nous allons vous expliquer comment en profiter.
+Pour effectuer la connexion de votre ordinateur en wifi vous pouvez suivre la procédure guidée de Windows.
+Cette opération est extrêmement rapide.
+Tout ce que vous avez à faire, est de suivre les étapes proposées et d'entrer les données qui vous sont demandées. <br> 
+Affichez les réseaux wifi disponibles
+Pour afficher les réseaux sans fil à proximité de votre ordinateur, cliquez sur l'icône wifi qui se situe au coin inferieur droit de votre écran (à côté de l'horloge Windows, voir image ci-dessous).<br>
+<img src="" alt="reseau"> <br><br>
+ 
+
+Retrouvez votre réseau
+En cliquant de nouveau avec le bouton droit sur l'icône des réseaux de Windows, choisissez de vous connecter à votre réseau. Si vous avez caché le SSID du réseau, choisissez la connexion manuelle et spécifiez dès à présent son nom.
+
+
+</p>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingThree">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree">PROTÉGER SES ENFANTS SUR INTERNET</button>                     
+            </h5>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingThree">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree">3. What is CSS?</button>                     
+            </h5>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h5 class="accordion-header" id="headingThree">
+                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree">3. What is CSS?</button>                     
+            </h5>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                <div class="card-body">
+                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+<!--<div class="accordion accordion-flush mb-3" id="accordionFlushExample">
 
   <div class="accordion-item">
     <h3 class="accordion-header" id="flush-headingOne">
@@ -34,7 +108,7 @@ include 'inc/nav.inc.php';
     <h3 class="accordion-header" id="flush-headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
         <div class="row">
-        <div class="col-1 bg-light text-center p-auto around">02</div>
+        <div class="col-1 bg-light text-center p-auto around ">02</div>
             <div class="col-11">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, vero natus dolore placeat iusto adipisci explicabo, cupiditate sed maiores suscipit numquam quisquam? Molestias sint excepturi non amet quas, sed libero.
             Labore, adipisci earum consequatur recusandae modi tempora itaque,</div>
         </div>
@@ -62,6 +136,7 @@ include 'inc/nav.inc.php';
     </div>
   </div>
 </div>
+-->
 
 
  
