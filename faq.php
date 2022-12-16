400b3894c557cdea7d80cc56417e6a09063e1f6b
ismaila2594 <ismaila.sagna@gmail.com>
2022-12-16 10:31:30 +0100

    Update faq.php

--- faq.php
+++ faq.php
@@ -8,15 +8,15 @@ include 'inc/nav.inc.php';
  
 <main class="container p-5">
 <h3>FAQ</h3>
-<h4>Vous avez des questions ? Et vous souhaitez avoir plus de precision sur votre demande ?</h4>
-<p>Veuillez verifier si votre question n'est pas parmis les question ci-dessous, sinon veullez nous fairevotres demande dans la page contact <!-- lien vers page contact--></p>
+<h4>Vous avez des questions ? Et vous souhaitez avoir plus de précision sur votre demande ?</h4>
+<p>Veuillez vérifier si votre question n'est pas parmis les question ci-dessous, sinon veullez nous faire votres demande dans la page <a href="">contact</a><!-- lien vers page contact--></p>
 
 
 <div class="accordion p-5" id="accordionExample1 ">
   <div class="row">
 
     <div class="col-md-6">
-      <div class="accordion-item">
+      <div class="accordion-item mb-3">
     <h3 class="accordion-header" id="headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
 Vous intervenez sous quels délais?      </button>
@@ -27,66 +27,67 @@ Vous intervenez sous quels délais?      </button>
       </div>
     </div>
   </div>
-  <div class="accordion-item">
+  <div class="accordion-item mt-3 mb-3">
     <h3 class="accordion-header" id="headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          What is a FAQ document?
+          Vos réparations sont elles garanties ?
       </button>
     </h3>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample1">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        Oui, Createch Informatique s'engage à réintervenir gratuitement si la même anomalie que pour laquelle nous sommes intervenus réapparait dans les 30 jours qui suivent notre intervention.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h3 class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          What are the top 10 interview questions?
+          Pouvez vous reparer mon ordinateur a distance?
       </button>
     </h3>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample1">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        Oui, nous pouvons intervenir à distance, à condition que votre connexion internet fonctionne. <br> Cepandant, nous priviliégions le interventions physiques afin de pouvoir faire un audit plus complet de l'équipement et d'avoir un contact humain avec nos clients.
       </div>
     </div>
   </div>
 </div> 
 
 <div class="col-md-6">
-      <div class="accordion-item">
+      <div class="accordion-item mb-3">
     <h3 class="accordion-header" id="headingFour">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
-          What is the purpose of a FAQ?
+          Pouvez vous m'aidez à installer mon nouveau PC?
       </button>
     </h3>
     <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample1">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        Oui, notre forfait installation informatique à domicile inclus le paramétrage de votre ordinateur ainsi que la connexion de celui ci à internet.
       </div>
     </div>
   </div>
-  <div class="accordion-item">
+  <div class="accordion-item mt-3 mb-3">
     <h3 class="accordion-header" id="headingFive">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-          What is a FAQ document?
+          Que dois-je payer si mon PC n'est pas réparé?
       </button>
     </h3>
     <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample1">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        Rien, aucun frais ne vous sera réclamé si votre panne n'est pas résolue. Cela fait partie de la garantie de resultat chez Createch Informatique.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h3 class="accordion-header" id="headingSix">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-          What are the top 10 interview questions?
+          Comment se passe une réparation information?
       </button>
     </h3>
     <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample1">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        Apres avoir pris rendez-vous avec nos services, nous envoyons un technicien à votre domicile. <br> Celui-ci effectue la réparation de votre ordinateur tout en vous donnant des informations sur l'origine de la panne et comment l'éviter à l'avenir. <br>
+        Si vous avez des questions, n'hésitez pas à les lui poser.
       </div>
     </div>
   </div>
