f8efe63d6d7795b4ae42ccf289dc6a8116dec591
ismaila2594 <ismaila.sagna@gmail.com>
2022-12-23 02:24:53 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,18 +6,56 @@ include 'inc/nav.inc.php';
 
 ?>
 
-<!-- test -->
 <div class="container">
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nom@example.com">
+<div class="row align-items-stretch no-gutters contact-wrap m-3 p-3">
+<div class="col-md-6">
+<div class="form h-100">
+<h3>Contactez nous</h3>
+<form class="mb-5" method="post" id="contactForm" name="contactForm">
+<div class="row">
+<div class="col-md-6 form-group mb-5">
+<label for="" class="col-form-label">Nom Prénom *</label>
+<input type="text" class="form-control" name="name" id="name" placeholder="Nom Prénom">
 </div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Exemple message ...</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+<div class="col-md-6 form-group mb-5">
+<label for="" class="col-form-label">Email *</label>
+<input type="text" class="form-control" name="email" id="email" placeholder="Votre email">
 </div>
 </div>
-
+<div class="row">
+<div class="col-md-6 form-group mb-5">
+<label for="" class="col-form-label">Téléphone</label>
+<input type="text" class="form-control" name="phone" id="phone" placeholder="Téléphone #">
+</div>
+<div class="col-md-6 form-group mb-5">
+<label for="" class="col-form-label">Entreprise</label>
+<input type="text" class="form-control" name="company" id="company" placeholder="Votre entreprise">
+</div>
+</div>
+<div class="row">
+<div class="col-md-12 form-group mb-5">
+<label for="message" class="col-form-label">Message *</label>
+<textarea class="form-control" name="message" id="message" cols="30" rows="4" placeholder="Ecrivez votre message"></textarea>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12 form-group">
+<input type="submit" value="Envoyé" class="btn btn-primary rounded-2 py-2 px-4">
+<span class="submitting"></span>
+</div>
+</div>
+</form>
+<!--<div id="form-message-warning mt-4"></div>
+<div id="form-message-success">Message envoyé</div>-->
+</div>
+</div>
+<div class="col-md-6">
+<div class="contact-info h-100 mx-auto pt-3 text-center" >
+<img src="https://cdn.pixabay.com/photo/2016/04/04/14/12/monitor-1307227_960_720.jpg" class="img-fluid rounded">
+</div>
+</div>
+</div>
+</div>
 
 <!-- TEST DE WALID -->
 <form action="formulaire.php" method="post">
